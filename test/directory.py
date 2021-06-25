17c4f946c4a5ceccff7ff538c10f653372af60d4
Arsh <58148717+Akalia1988@users.noreply.github.com>
2021-06-25 16:34:35 -0500

    python script to create folder remotely on LINUX

--- test/directory.py
+++ test/directory.py
@@ -1,1 +1,42 @@
 
+# I will be using paramike,getpass module . It will help me to execute remote shell commands.
+# http://www.fabfile.org/
+
+import paramiko
+import os
+import subprocess
+import sys
+import getpass
+
+ssh = paramiko.SSHClient()
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+ssh.connect("computer_name", username="username", password="password", allow_agent = False)
+
+def add_user():
+# Ask for the input
+username = input("Enter Username ")
+password = 123445
+
+def mkdir(folder_absolute_path):
+    """
+    creates new folder
+    """
+run('mkdir {0}'.format(folder_absolute_path))
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
